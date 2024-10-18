0316f4d6bbce922797dcc56eee73733696d56ef6
SOORAJ M S <soorajms4@gmail.com>
2024-10-18 16:58:02 +0530

    Time: 2 ms (100%), Space: 16.6 MB (28.5%) - LeetHub

---
+++ 3312-number-of-changing-keys/3312-number-of-changing-keys.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        s = s.upper()
+        cnt = 0
+        for i in range(len(s) - 1):
+            if s[i] != s[i+1]:
+                cnt += 1
+        return cnt
