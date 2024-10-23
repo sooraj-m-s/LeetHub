c959e89b7cfe54702ba1fd226743bd4b151690cc
SOORAJ M S <soorajms4@gmail.com>
2024-10-23 12:31:00 +0530

    Time: 2 ms (94.6%), Space: 16.6 MB (7.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def fib(n):
-            if n == 1:
-                return 1
-            elif n == 2:
-                return 2
-            prv = 1
-            crnt = 1
-            for i in range(n-1):
-                temp = prv + crnt
-                prv = crnt
-                crnt = temp
-            return crnt
-        return fib(n)
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        prv, crnt = 1, 1
+        for i in range(n-1):
+            temp = prv + crnt
+            prv = crnt
+            crnt = temp
+        return crnt
