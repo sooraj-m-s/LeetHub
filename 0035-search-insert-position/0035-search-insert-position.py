221a662f96279dc9791f10a2dfa2e8654514f3c4
SOORAJ M S <soorajms4@gmail.com>
2024-10-24 16:41:39 +0530

    Time: 0 ms (100%), Space: 17.3 MB (17.28%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,8 +1,6 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
+        if target not in nums:
             nums.append(target)
-            nums.sort()
-            return nums.index(target)
+        nums.sort()
+        return nums.index(target)
