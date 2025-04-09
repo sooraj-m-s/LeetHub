e9ace02c57c42cb1747f51212a3647124bca3732
SOORAJ M S <soorajms4@gmail.com>
2025-04-09 16:46:44 +0530

    Time: 3 ms (53.89%), Space: 17.8 MB (71.26%) - LeetHub

--- 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.py
+++ 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.py
@@ -1,5 +1,6 @@
 class Solution:
     def trimMean(self, arr: List[int]) -> float:
         arr.sort()
-        temp_li = arr[round(len(arr)*5/100):-round(len(arr)*5/100)]
-        return sum(temp_li)/len(temp_li)
+        l = (len(arr)*5)//100
+        temp = arr[l:-l]
+        return sum(temp) / len(temp)
